d687bf467a5412f4d49e3a0e7258ef564a408a39
Aniltmg463 <aniltamang462@gmail.com>
2025-02-12 20:25:24 +0545

    day7 Setup React project, added react-router-dom, reusable components (Header, Footer, Layout), and Bootstrap for styling

---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='bg-dark text-light p-3'>
+      <h1 className="text-center">All Right Reserved &copy; TechAnil</h1>
+    </div>
+  )
+}
+
+export default Header
---
+++ client/src/components/Layout/Layout.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Header from './Header'
+import Footer from './Footer'
+
+const Layout = ({children}) => {
+  return (
+    <div>
+        <Header/>
+        <main style={{ minHeight: "80vh"}}>{children}</main>
+        <Footer/>
+     
+    </div>
+  )
+}
+
+
+
+// const Layout = ( porps) => {
+//   return (
+//     <div>
+//         <Header/>
+//         <main style={{ minHeight: "80vh"}} > {porps.children}</main>
+//         <Footer/>
+     
+//     </div>
+//   )
+// }
+
+export default Layout
