d1db2a31d8e0153f2f0767add82360674962a97a
https://github.com/Aniltmg463/Tech-hub.git <aniltamang462@gmail.com>
2025-02-27 21:27:24 +0545

    day22 Built HomePage with dynamic products, category & price filters, and backend-integrated search

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,18 +1,145 @@
-import React from "react";
+import React, { useState, useEffect, use } from "react";
 import Layout from "../components/Layout/Layout";
-import { useAuth } from "../context/auth";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { Checkbox, Radio } from "antd";
+import { Prices } from "./../components/Prices";
 
 const HomePage = () => {
-  const [auth, setAuth] = useAuth();
+  const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [checked, setChecked] = useState([]);
+  const [radio, setRadio] = useState([]);
+
+  //get all categories
+  const getAllCategory = async () => {
+    try {
+      const { data } = await axios.get(
+        `${process.env.REACT_APP_API}/api/v1/category/get-category`
+      );
+      if (data?.success) {
+        setCategories(data?.category);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something weny wrong in getting category");
+    }
+  };
+
+  useEffect(() => {
+    getAllCategory();
+  }, []);
+
+  //get all product
+  const getAllProducts = async () => {
+    try {
+      const { data } = await axios.get(
+        `${process.env.REACT_APP_API}/api/v1/product/get-product`
+      );
+      setProducts(data.products);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  //filter by category
+  const handleFilter = (value, id) => {
+    let all = [...checked];
+    if (value) {
+      all.push(id);
+    } else {
+      all = all.filter((c) => c !== id);
+    }
+    setChecked(all);
+  };
+
+  // useEffect(() => {
+  //   getAllProducts();
+  // }, []);
+
+  useEffect(() => {
+    if (!checked.length || !radio.length) getAllProducts();
+  }, [checked.length, radio.length]);
+
+  useEffect(() => {
+    if (checked.length || radio.length) filterProduct();
+  }, [checked, radio]);
+
+  //get filterd product
+  const filterProduct = async () => {
+    try {
+      const { data } = await axios.post(
+        // `${import.meta.env.VITE_REACT_APP_API}/api/v1/product/product-filters`,
+        `${process.env.REACT_APP_API}/api/v1/product/product-filters`,
+        {
+          checked,
+          radio,
+        }
+      );
+      setProducts(data?.products);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <Layout
-      title="Home Page"
+      title="All Products - Best Offer"
       description="Welcome to the Tech Hub website, your go-to platform for the latest in technology."
       keywords="technology, programming, react, mern, seo"
       author="Tech Anil"
     >
-      <h1>Home Page</h1>
-      <pre>{JSON.stringify(auth, null, 4)}</pre>
+      {/* //   <h1>Home Page</h1>
+    //   <pre>{JSON.stringify(auth, null, 4)}</pre> */}
+      <div className="row mt-3">
+        <div className="col-md-3">
+          <h4 className="text-left md-10">Filter By Category</h4>
+          <div className="d-flex flex-column">
+            {categories?.map((c) => (
+              <Checkbox
+                key={c._id}
+                onChange={(e) => handleFilter(e.target.checked, c._id)}
+              >
+                {c.name}
+              </Checkbox>
+            ))}
+          </div>
+
+          {/* price filter */}
+          <h4 className="text-left mt-4">Filter By Prices</h4>
+          <div className="d-flex flex-column">
+            <Radio.Group onChange={(e) => setRadio(e.target.value)}>
+              {Prices?.map((p) => (
+                <div key={p._id}>
+                  <Radio value={p.array}>{p.name}</Radio>
+                </div>
+              ))}
+            </Radio.Group>
+          </div>
+        </div>
+        <div className="col-md-9">
+          {JSON.stringify(radio, null, 4)}
+          <h1 className="text-center">All Products</h1>
+          <div className="d-flex flex-wrap">
+            {products?.map((p) => (
+              <div className="card" style={{ width: "18rem" }}>
+                <img
+                  src={`${process.env.REACT_APP_API}/api/v1/product/product-photo/${p._id}`}
+                  className="card-img-top"
+                  alt={p.name}
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{p.name} </h5>
+                  <p className="card-text">{p.description.substring(0, 30)}</p>
+                  <p className="card-text">{p.price}</p>
+                  <button class="btn btn-primary ms-1">More Details</button>
+                  <button class="btn btn-secondary ms-1">ADD TO CART</button>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };
